583bf48fb3ef57678a44646c8edc078548c5b952
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-24 16:05:55 -0700

    jade loaded

--- models/index.js
+++ models/index.js
@@ -9,23 +9,25 @@ var sequelize 	= new Sequelize(config.database, config.username, config.password
 var db        	= {};
 
 
-fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf(".") !== 0) && (file !== "index.js");
-  })
-  .forEach(function(file) {
-    var model = sequelize.import(path.join(__dirname, file));
-    db[model.name] = model;
-  });
 
-Object.keys(db).forEach(function(modelName) {
-  if ("associate" in db[modelName]) {
-    db[modelName].associate(db);
-  }
-});
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+// fs
+//   .readdirSync(__dirname)
+//   .filter(function(file) {
+//     return (file.indexOf(".") !== 0) && (file !== "index.js");
+//   })
+//   .forEach(function(file) {
+//     var model = sequelize.import(path.join(__dirname, file));
+//     db[model.name] = model;
+//   });
+
+// Object.keys(db).forEach(function(modelName) {
+//   if ("associate" in db[modelName]) {
+//     db[modelName].associate(db);
+//   }
+// });
+
+// db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
 
 module.exports = db;

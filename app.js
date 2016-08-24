03d1898a935974a145b270df1b23c6d9ebdb5cc6
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-24 11:12:43 -0700

    nodeadmin

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ var app = express();
 var nodeadmin = require('nodeadmin');
 app.use(nodeadmin(app));
 
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
 
 
 // var http = require('http');

2866ea6f782d96f8904273afc1b6f18b5e0f2599
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-24 11:00:54 -0700

    nodeadmin

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Apache!\n');
-}).listen(8000, '127.0.0.1');
+var express= require('express');
+var app = express();
+
+var nodeadmin = requier('nodeadmin');
+app.use(nodeadmin(app));
+
+
+
+
+// var http = require('http');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello Apache!\n');
+// }).listen(8000, '127.0.0.1');

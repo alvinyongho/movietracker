42369c34910971186fe52728573b183b8efdc0d6
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-25 12:33:31 -0700

    movies listing

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var models = require('../models')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	models.Movie.findAll().then(function(movies) {
-    res.render('index', {
-      title: 'Movies',
-      movies: movies
-    });
-  });
-
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;

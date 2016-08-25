f6f80ad310f3dcb6e21ebcc93e42938af95e92d8
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-25 12:42:26 -0700

    movies listing

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,16 @@ var models = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  // models.Movie.findAll().then(function(movies) {
+  models.Movie.findAll().then(function(movies) {
     res.render('index', {
       title: 'Movies listing',
-      // movies: movies
-    // });
+      movies: movies
+    });
   });
 
-    (models.Movie.findAll().then(function(movies) {
-      console.log(JSON.stringify(movies))
-    }));
+    // (models.Movie.findAll().then(function(movies) {
+    //   console.log(JSON.stringify(movies))
+    // }));
 });
 
 module.exports = router;

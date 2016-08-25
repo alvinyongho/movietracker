7dc2529b699a34765d0e3d6898c0a0bb8994e44c
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-25 12:59:39 -0700

    movies listing

--- models/movie.js
+++ models/movie.js
@@ -1,7 +1,11 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var Movie = sequelize.define('Movie', {
-    movie_title: DataTypes.STRING
+    movie_title: DataTypes.STRING,
+    studio: DataTypes.STRING,
+    year: DataTypes.STRING,
+    box_office: DataTypes.STRING,
+    picture: DataTypes.STRING
   }, {
     classMethods: {
       associate: function(models) {

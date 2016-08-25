92d88f2af2f9f78cbbd0c7560ea65b069e001184
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-25 14:35:51 -0700

    cheerio

--- routes/movies.js
+++ routes/movies.js
@@ -48,7 +48,7 @@ router.post('/scrape', function(req, res) {
 
 
 
-          $('#body table:nth-child(3) tbody tr td:nth-child(1) table tbody tr').each(function(day) {
+          $('#body table tbody tr td table tbody tr').each(function(day) {
               $(this).find('td').each(function() {
                   console.log($(this).text());
               });

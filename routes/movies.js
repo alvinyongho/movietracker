06da148c73a2ec886a9814ce3c868871e11df84d
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-25 13:57:14 -0700

    okay lets scrape that site

--- routes/movies.js
+++ routes/movies.js
@@ -1,7 +1,6 @@
 var models  = require('../models');
 var express = require('express');
 var router  = express.Router();
-var request = require('requests');
 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -30,11 +29,12 @@ router.post('/scrape', function(req, res) {
   // }).then(function() {
 
 
+    var request = require('request');
     request('http://www.google.com', function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        console.log(body) // Show the HTML for the Google homepage.
+        console.log(body) // Show the HTML for the Google homepage. 
       }
-    })
+    });
 
     console.log("Running scraping");
     res.redirect('/movietracker');

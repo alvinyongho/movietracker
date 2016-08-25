f50463d6efbaa69fbce19428068672df0c72ec9e
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-25 14:01:45 -0700

    cheerio

--- routes/movies.js
+++ routes/movies.js
@@ -30,11 +30,25 @@ router.post('/scrape', function(req, res) {
 
 
     var request = require('request');
-    request('http://www.google.com', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log(body) // Show the HTML for the Google homepage. 
-      }
-    });
+    var i=0;
+    for (i=0; i<7; i++){
+      var url = "http://www.boxofficemojo.com/alltime/world/?pagenum=" + i + "&p=.htm"
+      request(url, function(err, resp, body) {
+        if (err)
+            throw err;
+        $ = cheerio.load(body);
+        console.log("CURRENTLY SCRAPING PAGE " + i);
+        // TODO: scraping goes here!
+      });
+
+    }
+
+
+    // request('http://www.google.com', function (error, response, body) {
+    //   if (!error && response.statusCode == 200) {
+    //     console.log(body) // Show the HTML for the Google homepage. 
+    //   }
+    // });
 
     console.log("Running scraping");
     res.redirect('/movietracker');

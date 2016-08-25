b6f5ec322301ef415a871b1946c6142c7205c0c2
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-25 10:40:32 -0700

    route for create movie

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/movietracker/users', users);
-app.use('/movietracker/movies', movies);
+app.use('/users', users);
+app.use('/movies', movies);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

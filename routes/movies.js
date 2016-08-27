ecf3d068a29d654bc316054d3f6851cef89563f5
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-26 17:58:56 -0700

    picture

--- routes/movies.js
+++ routes/movies.js
@@ -58,17 +58,11 @@ router.post('/addimages', function(req, res){
   models.Movie.findAll().then(function(movies) {
     for (movie in movies){
       
-
-      request({url:result_url, json:true}, (function(movie){
-
-        movie_title = movie.movie_title;
-        var result_url = url+movie_title;
-
-
-        return function (e, r, body) {
-          console.log(body['Search'][0]['Poster']);
-        }
-      })(movie));
+      movie_title = movie.movie_title;
+      var result_url = url+movie_title;
+      request.get({url:result_url, json:true}, function (e, r, body) {
+        console.log(body['Search'][0]['Poster']);
+      })
 
       // movie.update({
 

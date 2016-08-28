7d496beab8e480ab685ec612ce3b6f90aac1692f
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-27 20:47:54 -0700

    add destroy

--- routes/movies.js
+++ routes/movies.js
@@ -229,6 +229,7 @@ router.post('/:movie_id/update', multer({ storage: storage}).single('picture'), 
 
   console.log("@@@@ THE RESULT NAME OF UPDATE IS" + result_name);
   console.log("The movie title is " + req.body.movie_title)
+  console.log("The movie id is " + req.query.movie_id)
 
   models.Movie.update({
 

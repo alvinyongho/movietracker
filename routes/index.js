9f6593fc424b5104e77f9b6de3812467bc677e7f
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-29 10:13:58 -0700

    node implementation of server sided tables

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,21 @@ var QueryBuilder = require('datatable');
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  
+  var tableDefinition = {
+    sTableName: 'Movie'
+  };
+
+  var queryBuilder = new QueryBuilder(tableDefinition);
+
+  var requestQuery = {
+    iDisplayStart: 0,
+    iDisplayLength: 5
+  };
+
+  var queries = queryBuilder.buildQuery(requestQuery);
+
+
   models.Movie.findAll().then(function(movies) {
     res.render('index', {
       title: 'Movies listing',

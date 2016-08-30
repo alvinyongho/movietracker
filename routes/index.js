18e100170f5e1c7e5193aff4878c16ea598381f9
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-29 20:57:07 -0700

    node implementation of server sided tables

--- routes/index.js
+++ routes/index.js
@@ -36,6 +36,36 @@ router.get('/', function(req, res) {
   console.log("THE QUERIES VARIABLE@@@: " + queries);
 
 
+  // Connect to ymsql
+  var mysql      = require('mysql');
+  var connection = mysql.createConnection({
+    host     : '127.0.0.1',
+    user     : 'movieadmin',
+    password : 'cse135_Nodeapp',
+    database : 'movieapp'
+  });
+
+  connection.connect();
+
+  connection.query('SELECT * from < table name >', function(err, rows, fields) {
+    if (!err)
+      console.log('The solution is: ', rows);
+    else
+      console.log('Error while performing Query.');
+  });
+
+  connection.end();
+
+
+
+
+
+
+
+
+
+
+
   models.Movie.findAll({limit: 1}).then(function(movies) {
     
     res.render('index', {

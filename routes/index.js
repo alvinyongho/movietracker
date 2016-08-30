388d15f9f93dba4cfdb078287d3c3b7998d7d6bd
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-29 21:39:16 -0700

    node implementation of server sided tables

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = express.Router();
 var models = require('../models');
 
-var async = require('async'),
-    QueryBuilder = require('datatable');
 
 const scheme = {
 
@@ -21,44 +19,9 @@ const scheme = {
 /* GET home page. */
 router.get('/', function(req, res) {
 
-  var tableDefinition = {
-    sTableName: 'Movies',
-  };
+  
 
-  var queryBuilder = new QueryBuilder(tableDefinition);
-  var requestQuery = {
-    iDisplayStart: 0,
-    iDisplayLength: 5
-  };
-
-  var queries = queryBuilder.buildQuery(requestQuery);
-
-  console.log(queries);
-
-
-  // Connect to ymsql
-  var mysql      = require('mysql');
-  var pool = mysql.createPool({
-    host     : '127.0.0.1',
-    user     : 'movieadmin',
-    password : 'cse135_Nodeapp',
-    database : 'movieapp'
-  });
-
-
-
-  console.log(queries.recordsFiltered);
-
-
-
-
-
-
-
-
-
-
-  models.Movie.findAll({limit: 1}).then(function(movies) {
+  models.Movie.findAll().then(function(movies) {
     
     res.render('index', {
       title: 'Movies listing',

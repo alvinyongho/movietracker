8a5269ccb3237bed78345296dfb594b39a1e09e0
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-30 01:10:49 -0700

    node implementation of server sided tables

--- routes/index.js
+++ routes/index.js
@@ -86,13 +86,20 @@ router.get('/', function(req, res) {
     
     // console.log("RESULT DATA IS...." + stringified);
 
+    if(numrow == 'ALL'){
+      next_disabled: true;
+      prev_disabled: true;
+    }
+
     console.log("COUNT"+results.one);
     res.render('index', {
     title: 'Movies listing',
     movies: results.two,
     next_page: (parseInt(page)+1),
     prev_page: prev_page,
-    numrow: numrow
+    numrow: numrow,
+    next_disabled: next_disabled,
+    prev_disabled: prev_disabled
 
     // resulting_array: stringified
     });

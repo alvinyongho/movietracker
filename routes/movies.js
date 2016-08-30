3cc79b8344cd5616e98871de563847e6b3e6683b
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-29 23:06:55 -0700

    node implementation of server sided tables

--- routes/movies.js
+++ routes/movies.js
@@ -317,4 +317,56 @@ router.get('/:movie_id', function(req, res) {
 });
 
 
+
+
+router.get('/json', function(req, res) {
+  var result = {};
+
+  var result_data = [];
+  
+  res.setHeader('Content-Type', 'application/json');
+
+
+  async.parallel({
+    one: function(callback){
+        setTimeout(function(){
+          models.Movie.count().then(function(c) {
+
+            callback(null, c);
+
+          })
+        }, 1000);
+    },
+    two: function(callback){
+        setTimeout(function(){
+
+          models.Movie.findAll({limit: 10}).then(function(movies) {
+            callback(null, movies);
+          });
+            
+        }, 1000);
+    }
+  },
+  function(err, results) {
+
+    result_data = {
+        "draw": 1,
+        "recordsTotal": results.one,
+        "data": JSON.parse(JSON.stringify(results.two))
+    }
+    
+    var stringified = JSON.stringify(result_data);
+    
+    console.log("RESULT DATA IS...." + stringified);
+    
+    res.json(result_data);
+
+
+
+ 
+      // results is equal to: {one: 1, two: 2}
+  });
+});
+
+
 module.exports = router;

7d4d1d595552fe49ff179b4918726a8bd2e61cd9
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-29 20:19:35 -0700

    node implementation of server sided tables

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,6 @@ var express = require('express');
 var router = express.Router();
 var models = require('../models');
 
-var async = require('async');
-var QueryBuilder = require('datatable');
-
-var Serializer = require('sequelize-to-json');
-
-
 
 
 const scheme = {
@@ -27,29 +21,11 @@ const scheme = {
 router.get('/', function(req, res) {
   
 
-
-  var tableDefinition = {
-    sTableName: 'Movie'
-  };
-
-  var queryBuilder = new QueryBuilder(tableDefinition);
-
-  var requestQuery = {
-    iDisplayStart: 0,
-    iDisplayLength: 5
-  };
-
-  var queries = queryBuilder.buildQuery(requestQuery);
-
-
   models.Movie.findAll().then(function(movies) {
-    let serializer = new Serializer(models.Movie, scheme);
-    let postAsJSON = serializer.serialize(movies[0]);
-
+    
     res.render('index', {
       title: 'Movies listing',
-      movies: movies,
-      postAsJSON: movies
+      movies: movies
     });
   });
 

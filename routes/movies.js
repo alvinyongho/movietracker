e823301d56734e6a31216def7a4887f99871f53b
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-31 12:39:30 -0700

    adding validation

--- routes/movies.js
+++ routes/movies.js
@@ -75,7 +75,7 @@ router.post('/create', multer({ storage: storage, limits: file_limit }).single('
     function findExtension(extension) { 
       return extension;
     }
-    console.log(valid_extensions.find(path(req.file['filename'])));
+    console.log(valid_extensions.find(path.extname(req.file['filename'])));
 
     // check if the file type is not ok
 

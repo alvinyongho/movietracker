5f0231d6a17960d5a4095031124a34c86024f122
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-30 20:56:06 -0700

    adding validation

--- routes/movies.js
+++ routes/movies.js
@@ -11,7 +11,6 @@ var validator = require('validator');
 var upload_dir = 'public/images/uploads/';
 
 
-
 function renderError(res, err){
   res.render('invalid', {
       error_message: err
@@ -51,6 +50,13 @@ router.post('/create', multer({ storage: storage, limits: file_limit }).single('
   if(!validator.isCurrency(req.body.box_office)){
     renderError(res, "Invalid Currency. Make sure it's in the format including dollar symbol: " +  String.fromCharCode(36) +'100.00');
   }
+
+
+  if(!validator.isInt(req.body.year,{min: 1990, max: 2016})){
+    renderError(res, "Invalid Year. Make sure it's in the format: between 1990 and 2016");
+
+  }
+
   if(req.file){
 
     
